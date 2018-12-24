581985fd40895ed1531dec359bd00a74cf1cc595
SwordShieldMouse <alke8882000@gmail.com>
2018-12-23 22:22:13 -0700

    fixed some bugs but not all

--- helper.py
+++ helper.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import re
 from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
+import unicodedata
 
 SOS_token = 0 # start of string
 EOS_token = 1 # end of string
@@ -41,7 +42,7 @@ class DecoderRNN(nn.Module):
         self.hidden_size = hidden_size
 
         # can't we somehow share embeddings with the encoder?
-        self.embedding = n.Embedding(output_size, hidden_size)
+        self.embedding = nn.Embedding(output_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
         self.out = nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax(dim = 1)
@@ -65,7 +66,7 @@ class AttentionDecoderRNN(nn.Module):
         self.dropout_p = dropout_p
         self.max_length = max_length
 
-        self.embedding = n.Embedding(output_size, hidden_size)
+        self.embedding = nn.Embedding(output_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
         self.attn = nn.Linear(hidden_size * 2, max_length) # attention over the outputs of the encoder (the sentence)
 
@@ -111,40 +112,40 @@ class Vocab:
             self.add_word(word)
 
     def add_word(self, word):
-        if word not in self.word2index.keys():
-            self.word2index[word] = self.n_words
+        if word not in self.word2ix.keys():
+            self.word2ix[word] = self.n_words
             self.word2count[word] = 1
-            self.index2word[self.n_words] = word
+            self.ix2word[self.n_words] = word
             self.n_words += 1
         else:
             self.word2count[word] += 1
 
-def ixs_from_sentence(lang, sentence):
-    return [lang.word2ix[word] for word in sentence.split(' ')]
+def ixs_from_sentence(vocab, sentence):
+    return [vocab.word2ix[word] for word in sentence.split(' ')]
 
-def tensor_from_sentence(lang, sentence):
+def tensor_from_sentence(vocab, sentence):
     # returns column of indices of words in sentence
-    ixs = ixs_from_sentence(lang, sentence)
+    ixs = ixs_from_sentence(vocab, sentence)
     ixs.append(EOS_token)
     return torch.tensor(ixs).view(-1, 1)
 
-def tensors_from_pair(input_lang, output_lang, pair):
-    input_tensor = tensor_from_sentence(input_lang, pair[0])
-    target_tensor = tensor_from_sentence(output_lang, pair[1])
+def tensors_from_pair(input_vocab, output_vocab, pair):
+    input_tensor = tensor_from_sentence(input_vocab, pair[0])
+    target_tensor = tensor_from_sentence(output_vocab, pair[1])
     return (input_tensor, target_tensor)
 
 class Data(Dataset):
     def __init__(self):
         self.input_vocab, self.output_vocab, self.pairs = read_languages('eng', 'fra', True)
-        print("read in {0} language pairs").format(len(pairs))
+        print("read in {0} language pairs".format(len(self.pairs)))
         self.pairs = filter_pairs(self.pairs)
-        print("filtered to {} language pairs").format(len(pairs))
+        print("filtered to {} language pairs".format(len(self.pairs)))
         for pair in self.pairs:
             self.input_vocab.add_sentence(pair[0])
             self.output_vocab.add_sentence(pair[1])
         print("word count:")
-        print(input_vocab.name, input_vocab.n_words)
-        print(output_vocab.name, output_vocab.n_words)
+        print(self.input_vocab.name, self.input_vocab.n_words)
+        print(self.output_vocab.name, self.output_vocab.n_words)
 
     def __len__(self):
         # returns length of the pairs dataset
@@ -153,13 +154,13 @@ class Data(Dataset):
     def get_vocab_lens(self):
         # returns lengths of input, output vocabs
         # use ix2word to account for SOS and EOS tokens
-        return input_vocab.n_words, output_vocab.n_words
+        return self.input_vocab.n_words, self.output_vocab.n_words
 
     def get_names(self):
-        return input_vocab.name, output_vocab.name
+        return self.input_vocab.name, self.output_vocab.name
 
     def __getitem__(self, ix):
-        return pairs[ix]
+        return self.pairs[ix]
 
 
 # Turn a Unicode string to plain ASCII, thanks to
@@ -173,8 +174,8 @@ def unicode_to_ascii(s):
 # lower-case, trim, remove non-letter characters
 def normalize_string(s):
     s = unicode_to_ascii(s.lower().strip())
-    s = re.sub(r'[.!?]', r' \1', s) # leave a double space to separate sentences
-    s = re.sub(r'[^a-zA-Z.!?'], r' ', s)
+    s = re.sub(r'[.!?]', r'  ', s) # leave a double space to separate sentences
+    s = re.sub(r'[^a-zA-Z.!?]', r' ', s)
     return s
 
 def read_languages(lang1, lang2, reverse = False):
@@ -182,7 +183,7 @@ def read_languages(lang1, lang2, reverse = False):
 
     lines = []
 
-    with open("data/{0}-{1}.txt", encoding = 'utf-8').format(lang1, lang2) as f:
+    with open("data/{0}-{1}.txt".format(lang1, lang2), encoding = 'utf-8') as file:
         lines = [line.strip() for line in file]
 
     # split the lines into language pairs and normalize
@@ -199,7 +200,36 @@ def read_languages(lang1, lang2, reverse = False):
     return input_vocab, output_vocab, pairs
 
 def filter_pair(pair):
-    return len(p[0].split(' ')) < MAX_LENGTH and len(p[1].split(" ")) < MAX_LENGTH and p[1].startswith(eng_prefixes)
+    return len(pair[0].split(' ')) < MAX_LENGTH and len(pair[1].split(" ")) < MAX_LENGTH and pair[1].startswith(eng_prefixes)
 
 def filter_pairs(pairs):
     return [pair for pair in pairs if filter_pair(pair)]
+
+def evaluate(encoder, decoder, data, x, y, max_length = MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = tensor_from_sentence(data.input_vocab, x)
+        input_length = input_tensor.size(0)
+
+        encoder_outputs = torch.zeros((MAX_LENGTH, encoder.hidden_size))
+        encoder_hidden = encoder.reset_hidden()
+
+        for ix in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ix], encoder_hidden)
+            encoder_outputs[ix] = encoder_output[0]
+
+        decoder_input = torch.tensor([[SOS_token]])
+        decoder_hidden = encoder_hidden
+        decoded_words = []
+
+        for ix in range(MAX_LENGTH):
+            decoder_output, decoder_hidden, attn_weights = decoder(decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append("<EOS>")
+                break
+            else:
+                decoded_words.append(data.output_lang.ix2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words
--- main.py
+++ main.py
@@ -9,14 +9,14 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 
 # holds all the main classes
-import helper
+from helper import *
 
 data = Data()
 input_size, output_size = data.get_vocab_lens()
 input_lang, output_lang = data.get_names()
 
 batch_size = 10
-n_train = int(len(data) * 0.8)
+n_train = int(len(data) * 0.7)
 n_test = len(data) - n_train
 
 epochs = 3
@@ -40,12 +40,66 @@ encoder_optim = torch.optim.Adam(encoder.parameters(), lr = learning_rate)
 decoder_optim = torch.optim.Adam(decoder.parameters(), lr = learning_rate)
 attn_decoder_optim = torch.optim.Adam(attn_decoder.parameters(), lr = learning_rate)
 
+# train
 for _ in range(epochs):
-    for x, y in trainloader:
+    for batch_x, batch_y in trainloader:
         # x, y are batches of sentences from input, output language
-        encoder_hidden = encoder.reset_hidden()
-
+        loss = 0
         encoder_optim.zero_grad()
         attn_decoder_optim.zero_grad()
+        for batch in range(len(batch_x)):
+            encoder_hidden = encoder.reset_hidden()
 
-        
+            # convert the sentence pair to tensors
+            input_tensor, output_tensor = tensors_from_pair(data.input_vocab, data.output_vocab, (batch_x[batch], batch_y[batch]))
+
+            input_len = input_tensor.size(0)
+            output_len = output_tensor.size(0)
+
+            encoder_outputs = torch.tensor((MAX_LENGTH, hidden_size))
+
+            # run the encoder
+            for ix in range(input_len):
+                encoder_output, encoder_hidden = encoder(input_tensor[ix], encoder_hidden)
+                encoder_outputs[ix] = encoder_output[0]
+
+            # run the decoder
+            # feed the SOS token as input
+            decoder_input = torch.tensor([[SOS_token]])
+            # use the last hidden state of the encoder as the 1st hidden state of the decoder
+            decoder_hidden = encoder_hidden
+
+            if np.random.random() < teacher_forcing_ratio:
+                use_teacher_forcing = True
+            else:
+                use_teacher_forcing = False
+
+            if use_teacher_forcing is True:
+                for ix in range(output_len):
+                    decoder_output, decoder_hidden, attn_weights = attn_decoder(decoder_input, decoder_hidden, encoder_outputs)
+                    loss += criterion(decoder_output, target_tensor[ix])
+                    decoder_input = target_tensor[ix] # teacher forcing rather than actual output
+            else:
+                for ix in range(output_len):
+                    decoder_output, decoder_hidden, attn_weights = attn_decoder(decoder_input, decoder_hidden, encoder_outputs)
+                    topv, topi = decoder_output.topk(1)
+
+                    # detach from graph
+                    # can just be index since we pass it into an embedding
+                    decoder_input = topi.squeeze().detach() # no teacher forcing, so use the actual output
+
+                    loss += criterion(decoder_output, target_tensor[ix])
+                    if decoder_input.item() == EOS_token:
+                        break
+
+            loss.backward()
+
+            encoder_optim.step()
+            attn_decoder_optim.step()
+
+# evaluate
+for x, y in testloader:
+    pred = evaluate(encoder, attn_decoder, data, x, y)
+    print("Original is {}".format(x))
+    print("Translation is {}".format(' '.join(pred)))
+    break
